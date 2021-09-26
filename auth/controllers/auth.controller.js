e8ca0c495007709a46044a28a9edbf8be379939f
Глеб Барковский <gleb@MacBook-Pro-Gleb.local>
2021-09-26 15:57:03 +0300

    bugfix

--- auth/controllers/auth.controller.js
+++ auth/controllers/auth.controller.js
@@ -27,7 +27,7 @@ class AuthController {
             res.cookie('refreshToken', userData.refreshToken, {
                 maxAge: 30 * 24 * 60 * 60 * 1000,
                 httpOnly: true,
-                secure: process.env.SECURE
+                path: process.env.CLIENT_URL
             });
             return res.status(200).json(userData);
         } catch (error) {
