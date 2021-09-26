2c615e7d6779eec748bdef2aa7cae59d41b27102
Глеб Барковский <gleb@MacBook-Pro-Gleb.local>
2021-09-26 16:44:38 +0300

    bugfix

--- auth/controllers/auth.controller.js
+++ auth/controllers/auth.controller.js
@@ -32,7 +32,7 @@ class AuthController {
                 httpOnly: true,
                 secure: process.env.SECURE,
                 path: 'https://infallible-brattain-cf3a92.netlify.app',
-                sameSite: 'None',
+                SameSite: 'None',
             });
             return res.status(200).json(userData);
         } catch (error) {
