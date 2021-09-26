cbd1dcde34e9ef9e25c944d9bc669bdc3baa8b82
Глеб Барковский <gleb@MacBook-Pro-Gleb.local>
2021-09-26 18:18:41 +0300

    bugfix

--- auth/controllers/auth.controller.js
+++ auth/controllers/auth.controller.js
@@ -15,6 +15,7 @@ class AuthController {
                 maxAge: 30 * 24 * 60 * 60 * 1000,
                 httpOnly: true,
                 secure: process.env.SECURE,
+                domain: 'infallible-brattain-cf3a92.netlify.app',
                 sameSite: 'none',
             });
             return res.status(200).json(userData);
@@ -30,7 +31,7 @@ class AuthController {
                 maxAge: 30 * 24 * 60 * 60 * 1000,
                 httpOnly: true,
                 secure: process.env.SECURE,
-                site: 'https://murmuring-crag-87565.herokuapp.com',
+                domain: 'infallible-brattain-cf3a92.netlify.app',
                 sameSite: 'none',
             });
             return res.status(200).json(userData);
